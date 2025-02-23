a4f09234d1699b979377c972ed037dfaff18d77e
Basia Żygilewicz <basia.zygilewicz@gmail.com>
2025-02-23 20:56:40 +0100

    Fix header mobile layout

--- app/components/header/BottomBar.tsx
+++ app/components/header/BottomBar.tsx
@@ -7,7 +7,7 @@ const BottomBar = () => {
   return (
     <div className='border-b border-accent-primary/10 bg-light-bg-secondary backdrop-blur-sm dark:bg-dark-bg-secondary'>
       <div className='mx-auto'>
-        <div className='flex items-center justify-between px-4 py-4 md:px-6'>
+        <div className='flex items-center justify-between px-4 py-2 md:px-6'>
           <a href='/#home' className='group'>
             <Terminal size={22} className='text-accent-primary transition-colors dark:text-accent-light' />
           </a>
